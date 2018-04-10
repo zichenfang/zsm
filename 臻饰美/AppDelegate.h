1ae89c1c236db96d7db92a1213ba10de4c986750
fangzichen <zichenfang@yeah.net>
2018-04-10 19:52:45 +0800

    Initial Commit

---
+++ 臻饰美/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  臻饰美
+//
+//  Created by 方子辰 on 2018/4/10.
+//  Copyright © 2018年 方子辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
